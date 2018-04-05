2b538e6547da0b50a6538022fb0c5bf898de91f1
justPeterM8 <piotrg14@gmail.com>
2018-04-06 01:18:35 +0200

    Recyclerview for MainActivity

---
+++ app/src/main/java/students/polsl/eatnear/RestaurantsMainAdapter.java
@@ -0,0 +1,72 @@
+package students.polsl.eatnear;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.util.List;
+
+import students.polsl.eatnear.fake_data.FakeDataCreator;
+import students.polsl.eatnear.fake_data.Restaurant;
+
+
+public class RestaurantsMainAdapter extends RecyclerView.Adapter<RestaurantsMainAdapter.RestaurantTileViewHolder> {
+    private RestaurantTileListener restaurantListener;
+    private Context appContext;
+    private final List<Restaurant> fakeDataList;
+
+    public interface RestaurantTileListener{
+        void onClickAction(View view);
+    }
+
+    public RestaurantsMainAdapter(RestaurantTileListener listener, Context context, List<Restaurant> listOfRestaurants){
+        this.restaurantListener = listener;
+        this.appContext = context;
+        fakeDataList = listOfRestaurants;//filling with fake data
+    }
+
+    @Override
+    public RestaurantTileViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from((Context) appContext);
+        View view = layoutInflater.inflate(R.layout.restaurant_list, parent, false);
+        return new RestaurantTileViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RestaurantTileViewHolder holder, int position) {
+        //using fake data for now
+        holder.itemView.setTag(fakeDataList.get(position).getId());
+        holder.restaurantNameTW.setText(fakeDataList.get(position).getName());
+        holder.restaurantDistanceTW.setText(String.valueOf(fakeDataList.get(position).getDistance()));
+        holder.restaurantRating.setRating(fakeDataList.get(position).getRating());
+    }
+
+    @Override
+    public int getItemCount() {
+        return fakeDataList == null ? 0 : fakeDataList.size();
+    }
+
+    class RestaurantTileViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private TextView restaurantNameTW;
+        private TextView restaurantDistanceTW;
+        private RatingBar restaurantRating;
+
+        public RestaurantTileViewHolder(View itemView) {
+            super(itemView);
+            this.restaurantNameTW = itemView.findViewById(R.id.restaurantNameTextView);
+            this.restaurantDistanceTW = itemView.findViewById(R.id.distanceTextView);
+            this.restaurantRating = itemView.findViewById(R.id.ratingBar);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            restaurantListener.onClickAction(view);
+        }
+    }
+}
--- app/src/main/java/students/polsl/eatnear/tabs/NearRestaurantsFragment.java
+++ app/src/main/java/students/polsl/eatnear/tabs/NearRestaurantsFragment.java
@@ -1,22 +1,42 @@
 package students.polsl.eatnear.tabs;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import students.polsl.eatnear.R;
+import students.polsl.eatnear.RestaurantsMainAdapter;
+import students.polsl.eatnear.fake_data.FakeDataCreator;
 
-public class NearRestaurantsFragment extends Fragment{
+public class NearRestaurantsFragment extends Fragment implements RestaurantsMainAdapter.RestaurantTileListener{
+    private Context appContext;
+    private RecyclerView recyclerView;
+    private RestaurantsMainAdapter contactsAdapter;
+
     public NearRestaurantsFragment() {}
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_tab, container, false);
-
+        appContext = getActivity().getApplicationContext();
+        recyclerView = rootView.findViewById(R.id.restaurant_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(appContext));
+        contactsAdapter = new RestaurantsMainAdapter(this, appContext, FakeDataCreator.createRestaurantDataList(10));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(contactsAdapter);
         return rootView;
+    }
+
+    @Override
+    public void onClickAction(View view) {
+        Toast.makeText(appContext, "Click registered on tile: " + view.getTag(), Toast.LENGTH_SHORT).show();
     }
 }
