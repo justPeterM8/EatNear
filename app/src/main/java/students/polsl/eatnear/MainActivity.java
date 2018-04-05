305883dbe969428f07b2e23d0442b928d0258a8b
Patryk Dziura <patrykdziura8@gmail.com>
2018-04-05 18:06:08 +0200

    added tab selection support in main activity and fragments for each tab

--- app/src/main/java/students/polsl/eatnear/MainActivity.java
+++ app/src/main/java/students/polsl/eatnear/MainActivity.java
@@ -1,13 +1,65 @@
 package students.polsl.eatnear;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import students.polsl.eatnear.tabs.AllRestaurantsFragment;
+import students.polsl.eatnear.tabs.NearRestaurantsFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        mViewPager = findViewById(R.id.container);
+        TabLayout tabLayout = findViewById(R.id.tabs);
+
+        setSupportActionBar(toolbar);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+    }
+
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    NearRestaurantsFragment nearRestaurantsFragment = new NearRestaurantsFragment();
+                    return nearRestaurantsFragment;
+
+                case 1:
+                    AllRestaurantsFragment allRestaurantsFragment = new AllRestaurantsFragment();
+                    return allRestaurantsFragment;
+
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
     }
 }
